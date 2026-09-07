7820247f0c0c9adb4b29e2e28f51ecc1cff39890
pxlinspace <108901892+pxlinspace@users.noreply.github.com>
2026-09-06 20:43:10 -0700

    deathpolygonspawner now accounts for polygon offset + created level 1

--- scenes/spawners/death_polygon_spawner/death_polygon_spawner_temp.gd
+++ scenes/spawners/death_polygon_spawner/death_polygon_spawner_temp.gd
@@ -7,9 +7,17 @@ const DEATH_POLYGON_CONTAINER: PackedScene = preload("uid://3yrpbbdajl20")
 func get_spawn() -> Node2D:
 	var death_polygon_container := DEATH_POLYGON_CONTAINER.instantiate()
 	
-	for collision_polygon: CollisionPolygon2D in get_children():
+	for polygon_2d: Polygon2D in get_children():
+		var polygon: PackedVector2Array = polygon_2d.polygon
+		for i in polygon.size():
+			polygon[i] += polygon_2d.position
+		
+		var collision_polygon := CollisionPolygon2D.new()
+		collision_polygon.polygon = polygon
+		collision_polygon.build_mode = CollisionPolygon2D.BUILD_SEGMENTS
+
 		var death_polygon: Area2D = DEATH_POLYGON.instantiate()
-		death_polygon.add_child(collision_polygon.duplicate())
+		death_polygon.add_child(collision_polygon)
 		death_polygon_container.add_child(death_polygon)
 	
 	return death_polygon_container
