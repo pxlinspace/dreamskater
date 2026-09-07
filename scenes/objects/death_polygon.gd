512c2d195695a9c79d890ec682d4ff006a17ce86
pxlinspace <108901892+pxlinspace@users.noreply.github.com>
2026-09-06 23:01:06 -0700

    reorganized everything + colored walls

--- scenes/objects/death_polygon.gd
+++ scenes/objects/death_polygon.gd
@@ -5,4 +5,4 @@ func _draw() -> void:
 	for collision_polygon: CollisionPolygon2D in get_children():
 		var points: PackedVector2Array = collision_polygon.polygon
 		points.append(points[0])
-		draw_polyline(points, Color.WHITE, 4)
+		draw_polyline(points, Color("#593a4a"), 4)
