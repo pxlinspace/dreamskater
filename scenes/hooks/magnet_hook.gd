147a4656799614df9b2348f101c7df14b687799f
pxlinspace <108901892+pxlinspace@users.noreply.github.com>
2026-09-06 19:26:43 -0700

    magnet hook now disables collision when player is attached

--- scenes/hooks/magnet_hook.gd
+++ scenes/hooks/magnet_hook.gd
@@ -1,5 +1,13 @@
 extends Hook
 
+@onready var hook_line: HookLine = $HookLine
+@onready var death_area: Area2D = $DeathArea
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+
+func _on_player_exit_area_area_exited(_area: Area2D) -> void:
+	set_is_death(true)
+
 
 func get_type() -> Hook.Type:
 	return Type.MAGNET
@@ -14,8 +22,13 @@ func unhighlight() -> void:
 
 
 func show_line() -> void:
-	pass
+	hook_line.fade_in()
 
 
 func hide_line() -> void:
-	pass
+	hook_line.fade_out()
+
+
+func set_is_death(is_death: bool) -> void:
+	death_area.set_deferred("monitorable", is_death)
+	sprite_2d.modulate = Color.RED if is_death else Color.GREEN
