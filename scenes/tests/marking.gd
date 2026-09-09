d25b71f170666a56c835a21c9aaa003c6fa3ce8a
pxlinspace <108901892+pxlinspace@users.noreply.github.com>
2026-09-08 22:52:13 -0700

    changed closed shape test to also include intersections as a separate class

--- scenes/tests/marking.gd
+++ scenes/tests/marking.gd
@@ -2,11 +2,16 @@ class_name Marking extends Area2D
 
 const POINT_DISTANCE_SQUARED: float = 400.0
 var points: PackedVector2Array = []
+
 var intersected_indices: PackedInt32Array
+var intersection_indices: PackedInt32Array
 
 
 func add_point(point: Vector2) -> void:
 	points.append(point)
+	var size := points.size()
+	if size >= 5:
+		add_collision_segment(size-5, size-4)
 
 
 func set_last_point(point: Vector2) -> void:
@@ -14,8 +19,6 @@ func set_last_point(point: Vector2) -> void:
 	points[size-1] = point
 	if size >= 2 and point.distance_squared_to(points[size-2]) >= POINT_DISTANCE_SQUARED:
 		add_point(point)
-		if size > 2:
-			add_collision_segment(size-3, size-2)
 
 
 func add_collision_segment(index_a: int, index_b: int) -> void:
@@ -24,16 +27,21 @@ func add_collision_segment(index_a: int, index_b: int) -> void:
 	segment_shape.a = points[index_a]
 	segment_shape.b = points[index_b]
 	collision_shape.shape = segment_shape
-	add_child(collision_shape)
+	call_deferred("add_child", collision_shape)
 
 
 func add_intersected_marking(marking_index: int) -> void:
 	intersected_indices.append(marking_index)
 
+func add_intersection(intersection_index: int) -> void:
+	intersection_indices.append(intersection_index)
 
-func release() -> void:
+
+func release() -> void: #i'll make this better later lol
 	var size := points.size()
 	if size >= 1:
 		add_collision_segment(size-2, size-1)
 	if size > 2:
 		add_collision_segment(size-3, size-2)
+	if size > 3:
+		add_collision_segment(size-4, size-3)
--- scenes/tests/marking_test_2.gd
+++ scenes/tests/marking_test_2.gd
@@ -2,9 +2,12 @@ extends Node2D
 
 const MARKING: PackedScene = preload("uid://dd0c3y8d3jqy5")
 
+var intersections: Array[Intersection] = []
 var is_holding := false
+
 @onready var marking_container: Node2D = $MarkingContainer
 @onready var mouse_area: Area2D = $MouseArea
+@onready var mouse_area_segment_shape: SegmentShape2D = $MouseArea/CollisionShape2D.shape
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -21,9 +24,11 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func _physics_process(_delta: float) -> void:
+	print("number of intersections:", intersections.size())
 	if is_holding:
 		queue_redraw()
-		mouse_area.position = get_local_mouse_position()
+		mouse_area_segment_shape.b = mouse_area_segment_shape.a
+		mouse_area_segment_shape.a = get_local_mouse_position()
 		marking_container.get_child(-1).set_last_point(get_local_mouse_position())
 
 
@@ -31,15 +36,38 @@ func _draw() -> void:
 	for marking: Marking in marking_container.get_children():
 		if marking.points.size() > 1:
 			draw_polyline(marking.points, Color.WHITE, 2)
+	
+	for intersection in intersections:
+		draw_circle(intersection.position, 2, intersection.color)
+	
+	draw_line(mouse_area_segment_shape.a, mouse_area_segment_shape.b, Color.GREEN, 3)
 
 
-func _on_mouse_area_area_entered(intersected_marking: Marking) -> void:
+func _on_mouse_area_area_shape_entered(_area_rid: RID, other_marking: Marking, other_marking_shape_index: int, _local_shape_index: int) -> void:
 	if not is_holding:
 		return
 	var player_marking: Marking = marking_container.get_child(-1)
-	player_marking.add_intersected_marking(intersected_marking.get_index())
-	intersected_marking.add_intersected_marking(player_marking.get_index())
-	print(is_closed_shape(player_marking))
+
+	var other_marking_shape_owner: int = other_marking.shape_find_owner(other_marking_shape_index)
+	var other_marking_collision_shape: CollisionShape2D = other_marking.shape_owner_get_owner(other_marking_shape_owner)
+	var other_marking_segment_shape: SegmentShape2D = other_marking_collision_shape.shape
+
+	var intersection_position: Vector2 = Geometry2D.segment_intersects_segment(
+			other_marking_segment_shape.a, other_marking_segment_shape.b,
+			mouse_area_segment_shape.a, mouse_area_segment_shape.b
+	)
+
+	var intersection := Intersection.new()
+	intersection.position = intersection_position
+	intersection.marking_indices = [player_marking.get_index(), other_marking.get_index()]
+
+	intersections.append(intersection)
+	var intersection_index := intersections.size()-1
+
+	player_marking.add_intersection(intersection_index)
+	other_marking.add_intersection(intersection_index)
+
+	player_marking.add_point(intersection_position)
 
 
 func is_closed_shape(root_marking: Marking) -> bool:
@@ -48,6 +76,7 @@ func is_closed_shape(root_marking: Marking) -> bool:
 	var indices_found: PackedInt32Array = [root_index]
 	
 	return find_unique_indices(root_index, target_index, indices_found)
+
 
 func find_unique_indices(root_index: int, target_index: int, indices_found: PackedInt32Array) -> bool:
 	for intersected_index: int in marking_container.get_child(root_index).intersected_indices:
@@ -63,6 +92,12 @@ func find_unique_indices(root_index: int, target_index: int, indices_found: Pack
 		if find_unique_indices(intersected_index, target_index, indices_found):
 			return true
 	return false
+
+
+class Intersection:
+	var position: Vector2
+	var marking_indices: PackedInt32Array # an intersection will only ever have 2 markings
+	var color: = Color(randf(), randf(), randf()) # temp property for testing
 
 
 # next steps:
